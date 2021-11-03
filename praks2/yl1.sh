8a279dc097a5320ec3258e9514e266cef9f8f18c
Magnus Jaaska <magnus.jaaska@khk.ee>
2021-11-03 10:09:27 +0200

    Kommentaari lisamine

--- praks2/yl1.sh
+++ praks2/yl1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-#See on kommentaar
+#Skript teeb muutuja mille sisuks võtab käsu 'whoami' väljundi.
+#Edasi echob ilma newline-ta Tere, seejärel muutuja ning siis ! newline-ga.
 var=$(whoami)
 
 echo -n "Tere "
