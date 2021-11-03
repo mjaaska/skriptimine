7c0dfca5562759ac31791333050d20336382878d
Magnus Jaaska <magnus.jaaska@khk.ee>
2021-11-03 10:50:40 +0200

    yl3.2.sh

---
+++ praks3/yl3.1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo -n 'Sisesta oma ees- ja perenimi: '
+read varname
+echo Tere tulemast, $varname
+echo -n 'Sisesta oma ees- ja perenimi: '
+read nimi1 nimi2
+echo Sinu eesnimi on $nimi1 ja perenimi $nimi2
