6e371cc688d9eb69b691a435831ac705b60cb60a
Magnus Jaaska <magnus.jaaska@khk.ee>
2021-11-03 14:48:20 +0200

    Ajaarvesti vastavalt süsteemi kellaajale

---
+++ praks5/yl5.4.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#echo -n "Sisesta aja väärtus: "
+#read aja
+aja=$(date +%H)
+
+if test $aja -ge 6 -a $aja -lt 12; then
+	echo "Tere hommikust"
+elif test $aja -ge 12 -a $aja -lt 18; then	
+	echo "Tere päevast"
+elif test $aja -ge 18 -a $aja -lt 22; then
+	echo "Tere Õhtust"
+elif [ $aja -ge 22 -a $aja -lt 24 ] || [ $aja -ge 0 -a $aja -lt 6 ]; then
+	echo "Head ööd!"
+else
+	echo "Vale sisend"
+fi
