a769073e139283a135b311425886e98f287f79ec
Magnus Jaaska <jaaska@gmail.com>
2021-11-17 09:48:51 +0200

    Skript 7

---
+++ praks7/yl7.3.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+echo -n "Sisesta ridade arv: "
+read rida
+# echo -n "Sisesta tarnide arv reas: "
+# read tarn
+
+for ((  i=1 ; i <= rida; i++ ))
+do
+        echo -n "$i. "
+        for (( j = 1 ;j <=i ; j++ ))
+        do
+                echo -n "* "
+        done
+
+        echo ""
+done
